e4287e58e9dc79f115b7d8c5db142323c4a6cb7f
Judah Abraham <judahabrahamforgood@gmail.com>
2025-10-11 23:39:08 +0100

    Add z-index to Navbar for improved layering

--- Client/src/Components/Navbar.tsx
+++ Client/src/Components/Navbar.tsx
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 bg-[#1e293b] w-[100%] h-[70px] p-[2rem] 
-    flex items-center justify-between">
+    flex items-center justify-between z-50">
       <h1 className="text-2xl font-semibold text-white font-nunito sm:text-3xl">Jshare</h1>
       <a href="https://github.com/Judahabraham15/Jshare" target="_blank">
         <FaGithub className="text-3xl text-slate-400" />
